1ec59eab1fb61cf9397d57679f55f3b1e3bbd2e6
Brian Kimbrow <bkimbrow25@gmail.com>
2023-08-01 02:56:12 -0500

    update readme

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const weatherData = require("./data/weather.json"); // Use require to read the weather.json file
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express Server!");
+});
+
+app.get("/weather", (req, res) => {
+  res.json(weatherData); // Return the weather data as JSON
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
