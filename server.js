89fbf0c8af0992976d5a594c026c46bc361d9316
Brian Kimbrow <bkimbrow25@gmail.com>
2023-08-05 16:55:34 -0500

    node modules

--- server.js
+++ server.js
@@ -8,10 +8,43 @@ app.get("/", (req, res) => {
 });
 
 app.get("/weather", (req, res) => {
-  res.json(weatherData); 
-});
+ const newDataData = weatherData.map((data) => {
+    return{
+        city_name: data.city_name,
+        valid_date: data.valid_date,
+        description: data.description,
+    };
+    response.send(weatherData);
+ });
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+
+// app.get("./weather", (req, res) => {
+//     const{lat,lon, searchQuery} = request.searchQuery;
+// const cityForData = weatherData.find((data) => {
+//     return {
+//         data.city_name === city_name || 
+//         data.valid_date === valid_date ||
+//         Date.description == description
+//     }
+// })
+// })
+
+
+
+
+//   if (lat && lon) {
+//     const cityForData = weatherData.find((p) => p.lat === lat && p.lon === lon);
+//     if (cityForData) {
+//       return res.json(cityForData);
+//     } else {
+//       return res.status(404).json({ error: "City data not found." });
+//     }
+//   } else {
+//     return res.status(400).json({ error: "lat and lon parameters are required." });
+//   }
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
 
