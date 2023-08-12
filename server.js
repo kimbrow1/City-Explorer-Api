340dccd563c59a33baee489d3ed1bd14b54ed67c
Brian Kimbrow <bkimbrow25@gmail.com>
2023-08-12 14:28:44 -0500

    api

--- server.js
+++ server.js
@@ -1,33 +1,49 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3000;
-const weatherData = require("./data/weather.json");
+require("dotenv").config(); // will need anytime i use process.env express file 
+const port = process.env.PORT || 3001;
+const cors = require ('cors');
+const axios = require("axios");
+const WEATHERBIT = process.env.WEATHERBIT;
+
+
+
+class Forecast {
+  constructor (description, date) {
+    this.description = description;
+    this.date = date;
+  }
+}
+
+// It opens security on routes snd allows you to use middle ware
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, Server!");
 });
 
-app.get("/weather", (req, res) => {
- const newDataData = weatherData.map((data) => {
-    return{
-        city_name: data.city_name,
-        valid_date: data.valid_date,
-        description: data.description,
-    };
-    response.send(weatherData);
- });
 
-
-// app.get("./weather", (req, res) => {
-//     const{lat,lon, searchQuery} = request.searchQuery;
+app.get("/weather", async (req, res) => {
 // const cityForData = weatherData.find((data) => {
 //     return {
 //         data.city_name === city_name || 
 //         data.valid_date === valid_date ||
 //         Date.description == description
 //     }
-// })
-// })
+// }) 
+  try { 
+    const{lat,lon, cityName} = req.query;
+    const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily?key=${WEATHERBIT}&lat=${lat}&lon=${lon}&days=3`
+    );
+    weatherData.data.data;
+    const forecastArray = weatherData.data.data.map((item) => {
+      return new Forecast(item.weather.description, item.valid_date);
+    });
+    res.send(forecastArray);
+  } catch (error) {
+    res.status(500).send (error.message);
+  }
+});
 
 
 
@@ -44,7 +60,9 @@ app.get("/weather", (req, res) => {
 //   }
 // });
 
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
